d69306a9f35ff2de0d68206995798e8479bc3055
joss-bleach <joss.bleach@outlook.com>
2019-06-02 17:51:28 +0100

    database connected

--- src/app.js
+++ src/app.js
@@ -1,7 +1,16 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import keys from './config/keys';
 
 const app = express();
+
+//Database
+const db = keys.mongoUri;
+mongoose
+  .connect(db, { useNewUrlParser: true })
+  .then(() => console.log('Database connected...'))
+  .catch(err => console.log(err));
 
 //Middlewares
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -9,6 +18,7 @@ app.use(bodyParser.json());
 
 //Routes
 import router from './routes';
+import { Mongoose } from 'mongoose';
 app.use('/api', router);
 
 export default app;
---
+++ src/config/keys.js
@@ -0,0 +1,8 @@
+const keys = {
+  mongoUri:
+    'mongodb://devadmin:devpassword1@ds263876.mlab.com:63876/link-site-dev',
+  secretOrKey:
+    'iHPaMvxL0jPzb5VvYbGpwxux9VqLfieiV3VJoieMqoSIVnRZB4AVbVY_Fa8klWk8Ev-H_9zJ6v-kyGIfH5eNaAl4c0eal3FyqZ-NoXdBpSQwTTQPKdQWPNAJMMGsJzUKYeBZnXzSi2YdCLPEcgez2A89Bw9tLVHl74k1YjjxRuqx7dbnRNVZDPwtFH157SNz-NrNlaJKOn7lx6EeoUBCN3T6uXgY994qUaR7b1ZvtA4YCSfkRo0aGEiTdGj0K0DrXqo-gAosWgU_pkKKPXNRJQEs-tAsEYtY-zbwVjjItAYFqIQIIezrVXMGc0ZDS0qYQGenB1CpZKIJyvmZgMfQIQ'
+};
+
+export default keys;
